58000522f1fee98119b7504d4fe42ecb1b3f952c
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-09-23 14:58:56 +0100

    added github repo link

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <header className='w-full '>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img type='button'   src={Logo} alt="Github " />
-        <button className='black_btn' onClick={()=> window.open("https://github.com", "_blank")} >GitHub</button>
+        <button className='black_btn' onClick={()=> window.open("https://github.com/rajan7227/Summify", "_blank")} >GitHub</button>
       </nav>
       <h1 className="head_text">Summarize Article <br className='max-md:hidden' /> with <span className='orange_gradient'>OpenAI GPT-4</span></h1>
       <h2 className='desc'>Simplify your reading with Summize, an open-source article summarizer
