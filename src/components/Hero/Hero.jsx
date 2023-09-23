cb1e7c6b2bed163562a9939f685d1484bd1fcbb8
rajan7227 <114588581+rajan7227@users.noreply.github.com>
2023-09-23 14:52:44 +0100

    stage_one added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import  Logo from '../../assets/logo.svg'
+import Logo from '../../assets/logo.svg'
 
 const Hero = () => {
   return (
     <>
     <header className='w-full '>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
-        <img type='button'  src={Logo} alt="Github " />
+        <img type='button'   src={Logo} alt="Github " />
         <button className='black_btn' onClick={()=> window.open("https://github.com", "_blank")} >GitHub</button>
       </nav>
       <h1 className="head_text">Summarize Article <br className='max-md:hidden' /> with <span className='orange_gradient'>OpenAI GPT-4</span></h1>
